b1976bda2d2392068dc6fb5f1ae57a809264720a
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2026-03-16 23:58:50 +0800

    Update SpearPreferenceMissionBehavior.cs

--- SpearPreferenceMissionBehavior.cs
+++ SpearPreferenceMissionBehavior.cs
@@ -7,26 +7,34 @@ namespace SpearPreference
 {
 	public class SpearPreferenceMissionBehavior : MissionBehavior
 	{
+		private readonly Type _typeofStanceLogic;
+
 		public override MissionBehaviorType BehaviorType => MissionBehaviorType.Other;
 
-		public override void AfterStart()
+		public SpearPreferenceMissionBehavior()
 		{
-			if (SpearPreferenceSettings.Instance.ShouldOverrideRbmWeaponPreference)
+			_typeofStanceLogic = AppDomain.CurrentDomain.GetAssemblies().SelectMany(assembly => assembly.GetTypes()).FirstOrDefault(type => type.FullName == "RBMAI.StanceLogic");
+
+			if (_typeofStanceLogic != null && SpearPreferenceSettings.Instance.ShouldOverrideRbmWeaponPreference)
 			{
-				AppDomain.CurrentDomain.GetAssemblies().SelectMany(assembly => assembly.GetTypes()).FirstOrDefault(type => type.FullName == "RBMAI.StanceLogic")?.GetField("timeToUpdateAgents", BindingFlags.NonPublic | BindingFlags.Static)?.SetValue(null, float.PositiveInfinity);
+				// Override the interval for RBM's UpdateAgentStats call to an infinite amount of time.
+				_typeofStanceLogic.GetField("timeToUpdateAgents", BindingFlags.NonPublic | BindingFlags.Static)?.SetValue(null, float.PositiveInfinity);
 			}
 		}
 
 		public override void OnMeleeHit(Agent attacker, Agent victim, bool isCanceled, AttackCollisionData collisionData)
 		{
-			if (attacker != null && attacker.IsHuman && attacker.HasSpearCached)
+			if (_typeofStanceLogic == null || SpearPreferenceSettings.Instance.ShouldOverrideRbmWeaponPreference)
 			{
-				attacker.UpdateAgentStats();
-			}
+				if (attacker != null && attacker.IsHuman && attacker.HasSpearCached)
+				{
+					attacker.UpdateAgentStats();
+				}
 
-			if (victim != null && victim.IsHuman && victim.HasSpearCached)
-			{
-				victim.UpdateAgentStats();
+				if (victim != null && victim.IsHuman && victim.HasSpearCached)
+				{
+					victim.UpdateAgentStats();
+				}
 			}
 		}
 	}
